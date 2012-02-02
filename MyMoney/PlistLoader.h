b7dbe654592c34b9bc8b87893b57233bd21010b1
byeong cheol lim <lim6112j@gmail.com>
2012-02-03 02:21:34 +0900

    company name

--- MyMoney/PlistLoader.h
+++ MyMoney/PlistLoader.h
@@ -3,7 +3,7 @@
 //  DoodleFalling
 //
 //  Created by lim byeong cheol on 11. 9. 23..
-//  Copyright 2011년 SK M&S. All rights reserved.
+//  Copyright 2011년 ZenCom. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
